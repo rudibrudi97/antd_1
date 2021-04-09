3323e2d37e6f7328e073a3dbd64cd57d08c33314
DESKTOP-RB1ENME\user <beru1011@hs-karlsruhe.de>
2021-04-09 20:55:54 +0200

    Test_1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Sign from './steps';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Sign />
   </React.StrictMode>,
   document.getElementById('root')
 );
